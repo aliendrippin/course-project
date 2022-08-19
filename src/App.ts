c6f85313e7ba87f3791ce18bbffdf4964a4a8e69
Ty6epkyJIe3 <theevillkrolli@yandex.ru>
2022-08-19 09:46:52 +0300

    Финальный проект курса

---
+++ src/App.ts
@@ -0,0 +1,12 @@
+import {PromptService} from "./core/prompt/prompt.service";
+import {FfmpegExecutor} from "./commands/ffmpeg/ffmpeg.executor";
+import {ConsoleLogger} from "./out/console-logger/console-logger";
+
+export class App {
+    async run() {
+        new FfmpegExecutor(ConsoleLogger.getInstance()).execute();
+    }
+}
+
+const app = new App();
+app.run();
